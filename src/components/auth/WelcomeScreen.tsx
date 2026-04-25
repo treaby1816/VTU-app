c807c1662483e0e43b62ae72648a88fb25468c4e
Felix <treaby1816@gmail.com>
2026-04-25 10:48:20 +0100

    fix: welcome screen images show full faces on desktop

--- src/components/auth/WelcomeScreen.tsx
+++ src/components/auth/WelcomeScreen.tsx
@@ -30,7 +30,7 @@ export default function WelcomeScreen({ onGetStarted, onLogin, isMobile }: any) 
         }}>
           <img
             src={s.image} alt={s.title}
-            style={{ width: "100%", height: "100%", objectFit: "cover" }}
+            style={{ width: "100%", height: "100%", objectFit: "cover", objectPosition: "top center" }}
           />
         </div>
       ))}
